b800cd335e28073dc337157a1811d693517b99d7
David Grigorev <iu8david@gmail.com>
2024-06-08 21:10:55 +0600

    Коммит активити Search

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -1,6 +1,7 @@
 package com.practicum.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -13,6 +14,27 @@ class SettingsActivity : AppCompatActivity() {
         btBack.setOnClickListener {
             val backIntent = Intent(this, MainActivity::class.java)
             startActivity(backIntent)
+        }
+        val btShare = findViewById<Button>(R.id.bt_share)
+        btShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type="text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT,getString(R.string.share_application_message))
+            startActivity(shareIntent)
+        }
+        val btSupport = findViewById<Button>(R.id.bt_support)
+        btSupport.setOnClickListener {
+            val techSupportIntent = Intent(Intent.ACTION_SENDTO)
+            techSupportIntent.data = Uri.parse("mailto:")
+            techSupportIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.tech_support_email)))
+            techSupportIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.tech_support_email_title))
+            techSupportIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.tech_support_email_message))
+            startActivity(techSupportIntent)
+        }
+        val btUserAgreement = findViewById<Button>(R.id.bt_user_agreement)
+        btUserAgreement.setOnClickListener {
+            val userAgreementIntent = Intent(Intent.ACTION_VIEW,Uri.parse(getString(R.string.user_agreement_url)))
+            startActivity(userAgreementIntent)
         }
     }
 }
