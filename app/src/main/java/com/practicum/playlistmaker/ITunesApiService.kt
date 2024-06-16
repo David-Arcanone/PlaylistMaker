c253879b112d82f105f192697dcfa8c02f69b36f
David Grigorev <iu8david@gmail.com>
2024-06-16 21:33:48 +0600

    Работа с API iTunes поиск + заглушки(нет совпадений, ошибка)

--- app/src/main/java/com/practicum/playlistmaker/ITunesApiService.kt
+++ app/src/main/java/com/practicum/playlistmaker/ITunesApiService.kt
@@ -1,6 +1,5 @@
 package com.practicum.playlistmaker
 
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
--- app/src/main/java/com/practicum/playlistmaker/iTunesSearchResponse.kt
+++ app/src/main/java/com/practicum/playlistmaker/iTunesSearchResponse.kt
@@ -1,3 +1,3 @@
 package com.practicum.playlistmaker
 
-class iTunesSearchResponse (val results: Array<Track>, val resultCount: Int)
+class iTunesSearchResponse (val results: Array<Track>)
